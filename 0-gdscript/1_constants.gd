fb7c2db1bd87d2f9e02eb7f5ba5fa5345783bd7d
TON14 <ntarazanov.ton14@gmail.com>
2025-12-21 20:51:31 +0000

    constants example

---
+++ 0-gdscript/1_constants.gd
@@ -0,0 +1,15 @@
+extends Node
+
+# Typed Constants
+const TYPED_INTEGER: int = 14
+const TYPED_FLOAT: float = 14.14
+const TYPED_STRING: String = "Hello"
+
+const TYPED_VECTOR_2I: Vector2i = Vector2i(1, 2)
+const TYPED_VECTOR_2: Vector2 = Vector2(1.1, 2.2)
+
+const TYPED_VECTOR_3I: Vector3i = Vector3i(1, 2, 3)
+const TYPED_VECTOR_3: Vector3 = Vector3(1.1, 2.2, 3.3)
+
+const TYPED_VECTOR_4I: Vector4i = Vector4i(1, 2, 3, 4)
+const TYPED_VECTOR_4: Vector4 = Vector4(1.1, 2.2, 3.3, 4.4)
