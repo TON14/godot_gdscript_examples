6b80f59f829af79f6e3ce696a6e872ab1a6e691a
TON14 <ntarazanov.ton14@gmail.com>
2025-12-21 20:51:44 +0000

    array example

---
+++ 0-gdscript/2_array.gd
@@ -0,0 +1,15 @@
+extends Node
+
+# Use a single indentation level to distinguish continuation lines
+
+
+var typed_array: Array[int] = [
+	1,
+	2,
+	3,
+]
+
+func _ready() -> void:
+	print(typed_array[0])
+	#prints '1'
+	
