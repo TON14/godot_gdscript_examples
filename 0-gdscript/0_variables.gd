b146ea0a5ba50e783698ef8cc425d580c8e9e373
TON14 <ntarazanov.ton14@gmail.com>
2025-12-21 20:51:12 +0000

    variables example

---
+++ 0-gdscript/0_variables.gd
@@ -0,0 +1,15 @@
+# Typed Variables
+
+var typed_boolean: bool = true
+var typed_integer: int = 14
+var typed_float: float = 14.14
+var typed_string: String = "Hello"
+
+var typed_vector_2i: Vector2i = Vector2i(1, 2)
+var typed_vector_2: Vector2 = Vector2(1.1, 2.2)
+
+var typed_vector_3i: Vector3i = Vector3i(1, 2, 3)
+var typed_vector_3: Vector3 = Vector3(1.1, 2.2, 3.3)
+
+var typed_vector_4i: Vector4i = Vector4i(1, 2, 3, 4)
+var typed_vector_4: Vector4 = Vector4(1.1, 2.2, 3.3, 4.4)
